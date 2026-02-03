f4554066d049d433a40b207e56a2233923cb7f12
bbbbbbbIniad8 <0x7b123@gmail.com>
2026-02-03 16:49:09 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/turtle-image-code-gen-web",
+  assetPrefix: "/turtle-image-code-gen-web/",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
